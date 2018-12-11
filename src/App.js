8e30b511a3d384f8e51f912b175da724ba2f4165
Seongjun Lee <login@sjlee.org>
2018-12-11 05:30:27 -0800

    [STYLE] import style change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
-import SocialLink from './components/SocialLink.js';
+import SocialLink from './components/SocialLink';
 import './style/App.css';
 
 class App extends Component {
